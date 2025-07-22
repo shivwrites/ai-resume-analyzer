123ed12871d06aa04f8c20d1b41d84a937086c7b
shivwrites <ciaoshiv@gmail.com>
2025-07-22 12:16:40 +0530

    Fix Invalid Hook Call Error with Alias Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
+    alias: {
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+    },
     dedupe: ['react', 'react-dom'],
   },
 });
