a066774df909cd7212249019c1cdec6a8591263d
shivwrites <ciaoshiv@gmail.com>
2025-07-22 12:21:39 +0530

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
       react: path.resolve("./node_modules/react"),
