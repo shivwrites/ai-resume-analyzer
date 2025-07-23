4d70f05f6ac508a69790a6e816d4df970545b055
shivwrites <ciaoshiv@gmail.com>
2025-07-23 06:54:13 +0530

    Add SSR noExternal configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), reactRouter(), tsconfigPaths()],
+  ssr: {
+    noExternal: ['react', 'react-dom']
+  },
   resolve: {
     alias: {
       react: path.resolve("./node_modules/react"),
